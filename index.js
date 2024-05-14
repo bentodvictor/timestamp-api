de4e0dafd4632875128bc00b80b0a9b65c6e1802
Victor Bento <BRBENTOVI@tetrapak.com>
2024-05-14 15:46:28 -0300

    Fix input conditions

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const app = express();
 
 // Enabel CORS
 const cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));
+app.use(cors({ optionsSuccessStatus: 200 }));
 
 // URL-encoded data will be parsed with the querystring library.
 // Parses the JSON string in the request body and exposes it in the req.body property
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: "false"}));
+app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(bodyParser.json());
 
 // Set style
@@ -18,27 +18,24 @@ app.use(express.static('public'));
 app.get("/", (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.get("/api/:date?", (req, res) => {
-    let result = {};
     let dateToShow = new Date();
-    const {date} = req.params;
+    const { date } = req.params;
 
-    if(!date.includes("-"))
+    if (/^\d+$/.test(date))
         dateToShow = new Date(parseInt(date));
-    else 
+    else if (date == "" || date == null)
+        dateToShow = new Date();
+    else
         dateToShow = new Date(date);
 
-    if(dateToShow == 'Invalid Date') {
-        const actualTimeStamp = new Date();
-
-        result["unix"] = actualTimeStamp.valueOf(), 
-        result["utc"] = actualTimeStamp.toString()
-    }
-    else {
-        result["unix"] = dateToShow.valueOf(), 
-        result["utc"] = dateToShow.toString()
+    if (dateToShow == 'Invalid Date') {
+        res.json({ error: "Invalid Date" });
     }
 
-    res.send(result);
+    res.json({
+        unix: dateToShow.valueOf(),
+        utc: dateToShow.toUTCString()
+    });
 });
 
 
